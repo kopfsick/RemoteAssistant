b66d4e9ed41c31e5589232dfd97b72013917b1cc
Julian Hanssen <julian@kopfsick.com>
2018-06-08 09:33:02 +0200

    Added basic process apis
    Added ProcessController with ability to get all running processes and check if specific process is running

---
+++ src/RemoteAssistant.API/Controllers/ProcessController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RemoteAssistant.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProcessController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<object[]>> GetRunningProcesses()
+        {
+            var processes = GetAllProcesses();
+
+            return Ok(processes.Select(p => (p.ProcessName, p.ToString())));
+        }
+
+        [HttpGet("{isRunning}")]
+        public async Task<ActionResult<bool>> Status(string name)
+        {
+            return Ok(Process.GetProcessesByName(name).Any());
+        }
+
+        private static Process[] GetAllProcesses() => Process.GetProcesses();
+    }
+}

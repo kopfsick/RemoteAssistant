1e2d42215fb9adf402d37e99f47a27beef0a053b
Julian Hanssen <julian@kopfsick.com>
2018-06-08 10:20:15 +0200

    ProcessController refactoring
    Fixed routes (they were specified with HttpGet and not Route, which made them behave like parameters)
    
    Added initial implementations to kill/start processes

--- src/RemoteAssistant.API/Controllers/ProcessController.cs
+++ src/RemoteAssistant.API/Controllers/ProcessController.cs
@@ -20,10 +20,44 @@ namespace RemoteAssistant.API.Controllers
             return Ok(processes.Select(p => (p.ProcessName, p.ToString())));
         }
 
-        [HttpGet("{isRunning}")]
-        public async Task<ActionResult<bool>> Status(string name)
+        [Route("isrunning"),HttpGet]
+
+        public async Task<ActionResult<bool>> IsProcessRunning(string name)
         {
             return Ok(Process.GetProcessesByName(name).Any());
+        }
+
+        [Route("kill"), HttpGet]
+        public async Task<ActionResult<bool>> KillProcess(string name)
+        {
+            var processes = Process.GetProcessesByName(name);
+
+            foreach (var process in processes)
+                process.Kill();
+
+            return Ok(processes.Any());
+        }
+
+        [Route("start"), HttpGet]
+        public async Task<ActionResult<bool>> StartProcess(string name, bool allowMultiple = false)
+        {
+            //TODO rename 'name' to 'path'?
+
+            //TODO Need to check if file path exists
+            //TODO What to return when file path doesn't exist?
+
+            //TODO Can't check if process is running by using 'IsProcessRunning', it checks by name.
+            //TODO Need to check by full path? 'name'/'filepath' could be relative ("calc.exe"/"calc")
+
+            var startedNewProcess = false;
+
+            if (allowMultiple || !(await IsProcessRunning(name)).Value)
+            {
+                Process.Start(name);
+                startedNewProcess = true;
+            }
+
+            return Ok(startedNewProcess);
         }
 
         private static Process[] GetAllProcesses() => Process.GetProcesses();
